69d1e33dde11731ffc07a7618aaac0933c1c94f5
abhijeet-qsofte <abhijeet@qsofte>
2023-02-11 00:03:59 +1300

    QPW-5  adding some basic extensions and settings

--- src/components/navbar/navbarelements.js
+++ src/components/navbar/navbarelements.js
@@ -3,23 +3,24 @@ import { Link } from "gatsby"
 import { DiScala } from "react-icons/Di"
 
 export const Nav = styled.nav`
-    background: ${({ active }) =>
-      active
-        ? "#fff"
-        : "linear-gradient(to bottom, rgba(255,255,255, 0.9) 0%, rgba(255,255,255,0) 100% ) "};
-    height: 80px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 1rem
-    postion: sicky;
-    top: 0;
-    z-index: 999;
+  background: ${({ active }) =>
+    active
+      ? "#fff"
+      : "linear-gradient(to bottom, rgba(255,255,255, 0.9) 0%, rgba(255,255,255,0) 100% ) "};
+  height: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1rem;
+  postion: sicky;
+  top: 0;
+  z-index: 999;
 
-    @media screen and (max-width: 960px) {
-        background: ${({ click }) => (click ? "#fff" : "transparent")};
-        transition: 0.8s all ease;
-    }`
+  @media screen and (max-width: 960px) {
+    background: ${({ click }) => (click ? "#fff" : "transparent")};
+    transition: 0.8s all ease;
+  }
+`
 
 export const NavbarContainer = styled.div`
   display: flex;
